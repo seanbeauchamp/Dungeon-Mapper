0263fbecd89c4b6c56e5231490c66be77aaacb01
Sean <sean.marc.beauchamp@gmail.com>
2020-05-07 14:56:26 -0300

    Properties Card Header replaced with tabs, allows user to change properties of specific grid square. Installed "classnames" module.

--- src/components/PropertyCard.js
+++ src/components/PropertyCard.js
@@ -1,7 +1,9 @@
 import React, {Component} from 'react';
-import {Card, CardBody, CardHeader, Form, FormGroup,
-    Input, Label} from 'reactstrap';
+import {Card, CardBody, Form, FormGroup,
+    Input, Label, Nav, NavItem, NavLink, TabContent, TabPane,
+    Row, Col} from 'reactstrap';
 import styled from 'styled-components';
+import classnames from 'classnames';
 
 import BorderSelector from './BorderSelector';
 
@@ -11,34 +13,68 @@ const CustomBordersField = styled.fieldset`
 `;
 
 class PropertyCard extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            activeTab: '2',
+        }
+    }
+
+    setTab = (tabIndex) => {
+        this.setState({activeTab: tabIndex});
+    }
 
     render(){
         return(
             <Card>
-                <CardHeader>Settings</CardHeader>
                 <CardBody>
-                    <div className="optionsSection pl-2">
-                        <Form>
-                            <FormGroup>
-                                <Label>
-                                <Input
-                                    type="checkbox"
-                                    name="autoexpand"
-                                    defaultChecked={this.props.autoExpandSquares}
-                                    onChange={this.props.toggleAutoExpandSquares}
-                                    />
-                                    Auto-Expand/Retract Grid with Neighbour Squares
-                                </Label>
-                                <CustomBordersField expand={this.props.autoExpandSquares}>
-                                    <Label>
-                                        Custom Border Sides:
-                                        <BorderSelector expand={this.props.autoExpandSquares}
-                                            updatePresets={this.props.updatePresets} />
-                                    </Label>
-                                </CustomBordersField>
-                            </FormGroup>
-                        </Form>
-                    </div>
+                    <Nav tabs>
+                        <NavItem>
+                            <NavLink
+                              className={classnames({active: this.state.activeTab === '1'})}
+                              onClick={() => this.setTab('1')}
+                              >Current Cell</NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink
+                              className={classnames({active: this.state.activeTab === '2'})}
+                              onClick={() => this.setTab('2')}
+                            >Settings</NavLink>
+                        </NavItem>
+                    </Nav>
+                    <TabContent activeTab={this.state.activeTab}>
+                    <TabPane tabId="1">
+                        <Row>
+                            <Col sm="12">
+                                <h4 className="pt-4">No Cell Selected</h4>
+                            </Col>
+                        </Row>
+                    </TabPane>
+                    <TabPane tabId="2">
+                            <div className="optionsSection pl-2 pt-4">
+                                <Form>
+                                    <FormGroup>
+                                        <Label className="pl-3">
+                                        <Input
+                                            type="checkbox"
+                                            name="autoexpand"
+                                            defaultChecked={this.props.autoExpandSquares}
+                                            onChange={this.props.toggleAutoExpandSquares}
+                                            />
+                                            Auto-Expand/Retract Grid
+                                        </Label>
+                                        <CustomBordersField expand={this.props.autoExpandSquares}>
+                                            <Label>
+                                                Custom Border Sides:
+                                                <BorderSelector expand={this.props.autoExpandSquares}
+                                                    updatePresets={this.props.updatePresets} />
+                                            </Label>
+                                        </CustomBordersField>
+                                    </FormGroup>
+                                </Form>
+                            </div>
+                    </TabPane>
+                    </TabContent>
                 </CardBody>
             </Card>
         );
