5faadcaa5618072458bc2af9416a024f3b74a04a
Sean Beauchamp <sean.marc.beauchamp@gmail.com>
2020-06-22 22:27:09 -0300

    Attemping to add build folder to properly implement github-pages.

---
+++ build/precache-manifest.af2c1d26c78e34b89957d68ab729d654.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7998caeb0e6e75ece60fb78a004540f",
+    "url": "/Dungeon-Mapper/index.html"
+  },
+  {
+    "revision": "18b0590db2d520eedee2",
+    "url": "/Dungeon-Mapper/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "9987e71d595fbc1499c0",
+    "url": "/Dungeon-Mapper/static/css/main.8ab6f918.chunk.css"
+  },
+  {
+    "revision": "18b0590db2d520eedee2",
+    "url": "/Dungeon-Mapper/static/js/2.2159084b.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Dungeon-Mapper/static/js/2.2159084b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9987e71d595fbc1499c0",
+    "url": "/Dungeon-Mapper/static/js/main.6fdf1e22.chunk.js"
+  },
+  {
+    "revision": "a8ed3313ef277e2d5d10",
+    "url": "/Dungeon-Mapper/static/js/runtime-main.ef03fe71.js"
+  }
+]);
